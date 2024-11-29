42f50b13b321c02b142f54a51f18c4a6ab8c472b
Fariandi Ramadhan <fariandi.ramadhann@gmail.com>
2024-11-29 11:30:51 +0700

    modify: using index.php to connect to database

--- php/index.php
+++ php/index.php
@@ -1,2 +1,11 @@
 <?php
-phpinfo();
+// phpinfo();
+$conn = mysqli_connect("db", "root", "root", "db_docker", "3306") or die("cannot connect to the database!");
+
+$query = mysqli_query($conn, "select * from belajar_docker_php");
+
+$results = [];
+while($result = mysqli_fetch_assoc($query)){
+    array_push($results, $result);
+}
+echo json_encode($results);
